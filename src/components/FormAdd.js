0621e6986dd448732809f92e2a26a968f135b66b
DimaProCrypto <budanovdm@yahoo.com>
2024-08-06 14:57:42 -0700

    New page

--- src/components/FormAdd.js
+++ src/components/FormAdd.js
@@ -1,31 +1,35 @@
-"use client"
-
 import { useEffect, useState } from "react";
 import TelegramProvider from "./TelegramProvider";
 
 const FormAdd = () => {
-    const [tg, setTg] = useState({});
+  const [tg, setTg] = useState({});
 
-    useEffect(() => {
-        if(window !== undefined && window.Telegram && window.Telegram.WebApp) {
-            const tgData = window.Telegram.WebApp;
-            setTg(tgData);
-        } 
-    }, []);
-
-    const onClickCloseBot = () => {
-        tg.close();
+  useEffect(() => {
+    function initTg() {
+      if (window !== undefined && window.Telegram && window.Telegram.WebApp) {
+        const tgData = window.Telegram.WebApp;
+        setTg(tgData);
+      } else {
+        console.log("Telegram WebApp is undefined, retrying…");
+        setTimeout(initTg, 500);
+      }
     }
+    initTg();
+  }, []);
 
-    return (
-        <>
-            <TelegramProvider />
-            <div>
-                <button onClick={onClickCloseBot}>Закрыть</button>
-                <h2>Пользователь</h2>
-            </div>
-        </>
-    );
-}
+  const onClickCloseBot = () => {
+    tg.close();
+  }
+
+  return (
+    <>
+      <TelegramProvider />
+      <div>
+        <button onClick={onClickCloseBot}>Закрыть</button>
+        <h2>Пользователь</h2>
+      </div>
+    </>
+  );
+};
 
 export default FormAdd;
