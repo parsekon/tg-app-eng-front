76b7124110c16a51dfc5562a2b5a63948cdec0f0
DimaProCrypto <budanovdm@yahoo.com>
2024-08-06 14:05:25 -0700

    New page

---
+++ app/add/page.js
@@ -0,0 +1,11 @@
+import styles from '@/app/add/add.module.css';
+
+const Add = () => {
+    return (
+        <>
+            <h1 className={styles.title}>You can add a new phrase in list.</h1>
+        </>
+    )
+}
+
+export default Add;
