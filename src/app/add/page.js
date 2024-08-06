c10487fe2eedbce20ea8b4ec3c714682130902ac
DimaProCrypto <budanovdm@yahoo.com>
2024-08-06 14:29:22 -0700

    New page

---
+++ src/app/add/page.js
@@ -0,0 +1,13 @@
+import styles from '@/app/add/add.module.css';
+
+const Add = () => {
+    return (
+        <>
+            <div>
+                <h1 className={styles.title}>Добавить новую фразу:</h1>
+            </div>
+        </>
+    )
+}
+
+export default Add;
