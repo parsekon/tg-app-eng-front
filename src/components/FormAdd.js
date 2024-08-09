c351d5f31c3396deb4dfe5ae17d885db11fd4764
DimaProCrypto <budanovdm@yahoo.com>
2024-08-09 16:04:01 -0700

    Try

--- src/components/FormAdd.js
+++ src/components/FormAdd.js
@@ -5,72 +5,86 @@ import TelegramProvider from "./TelegramProvider";
 
 const FormAdd = () => {
   const [tg, setTg] = useState({});
-  const [eng, setEng] = useState('');
-  const [rus, setRus] = useState('');
+  const [eng, setEng] = useState("Hello");
+  const [rus, setRus] = useState("Привет");
 
   let tgData;
   useEffect(() => {
-      if (window !== undefined && window.Telegram && window.Telegram.WebApp) {
-         tgData = window.Telegram.WebApp;
-      }
-      setTg(tgData)
-      console.log("tg1>>", tg)
+    if (window !== undefined && window.Telegram && window.Telegram.WebApp) {
+      tgData = window.Telegram.WebApp;
+
+    }
+    setTg(tgData);
+    console.log("tg1>>", tgData);
   }, [tg]);
 
   const onClickCloseBot = () => {
     const data = {
       success: "By-By!!",
-    }
+    };
     tg.close();
     tg.sendData(JSON.stringify(data));
-    console.log("tg>>>>>>>>", tg)
+    console.log("tg>>>>>>>>", tg);
   };
+
 
   // const onSendWord = useCallback(() => {
   //   const data = {
-  //     eng: 'Hello',
-  //     rus: "Привет",
+  //     eng,
+  //     rus
   //   }
   //   console.log("TG before>>>", tgData)
   //   tg.onSendData(JSON.stringify(data));
-  // }, []);
+  // }, [eng, rus, tg]);
 
-//   useEffect(() => {
-//     tg?.onEvent('mainButtonClicked', onSendData); 
-//   return () => {
-//     tg?.offEvent('mainButtonClicked', onSendData);
-//   }
-// }, [tg]);
+  //   useEffect(() => {
+  //     tg.onEvent('mainButtonClicked', onSendWord);
+  //   return () => {
+  //     tg.offEvent('mainButtonClicked', onSendWord);
+  //   }
+  // }, [onSendWord, tg]);
 
-// useEffect(() => {
-//   tg.MainButton.setParams({
-//     text: 'Send data'
-//   });
-// }, [])
+  // useEffect(() => {
+  //   tg.MainButton.setParams({
+  //     text: 'Send data'
+  //   });
+  // }, [])
 
-// useEffect(() => {
-//   if(!eng || !rus) {
-//     tg.MainButton.hide();
-//   } else {
-//     tg.MainButton.show();
-//   }
-// }, [eng, rus])
+  // useEffect(() => {
+  //   if(!eng || !rus) {
+  //     tg.MainButton.hide();
+  //   } else {
+  //     tg.MainButton.show();
+  //   }
+  // }, [eng, rus])
 
-const onChangeEng = (e) => {
-  setEng(e.target.value);
-}
+  const onChangeEng = (e) => {
+    setEng(e.target.value);
+  };
 
-const onChangeRus = (e) => {
-  setRus(e.target.value);
-}
+  const onChangeRus = (e) => {
+    setRus(e.target.value);
+  };
 
   return (
     <>
       <TelegramProvider />
       <button onClick={onClickCloseBot}>Закрыть</button>
       <h1>Введите фразу:</h1>
-      <input className={"input"} type="text" placeholder={"Фраза"} value={eng} onChange={onChangeEng} />
-      <input className={"input"} type="text" placeholder={"Перевод"} value={rus} onChange={onChangeRus} />
+      <input
+        className={"input"}
+        type="text"
+        placeholder={"Фраза"}
+        value={eng}
+        onChange={onChangeEng}
+      />
+      <input
+        className={"input"}
+        type="text"
+        placeholder={"Перевод"}
+        value={rus}
+        onChange={onChangeRus}
+      />
     </>
   );
 };
--- src/components/TelegramProvider.js
+++ src/components/TelegramProvider.js
@@ -1,11 +1,22 @@
 "use client"
 
 import Script from "next/script";
+import { useEffect } from "react";
 
 const TelegramProvider = () => {
+
+    useEffect(() => {
+        // Проверяем, что Telegram Web App объект доступен
+        if (window.Telegram?.WebApp) {
+            const tg = window.Telegram.WebApp;
+            // Инициализация и настройка вашего приложения
+            tg.ready();  // Сообщает, что приложение готово
+        }
+    }, []);
+
     return (
         <>
-            <Script src="https://telegram.org/js/telegram-web-app.js" />
+            <Script src="https://telegram.org/js/telegram-web-app.js"  />
         </>
     );
 }
